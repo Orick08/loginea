4547c27d277c26c4901d654e1a50e537057a61e4
Erick Amezcua <erickamezcua12@gmail.com>
2021-06-05 11:44:15 -0600

    Adds token validation

--- api/routes/users.js
+++ api/routes/users.js
@@ -30,14 +30,37 @@ router.post('/singin', (req, res) => {
 
       if (rows.length > 0) {
         let data = JSON.stringify(rows[0]);
-        const TOKEN = jwt.sign(data, process.env.SECRET_KEY);
-        res.json({ TOKEN });
+        const token = jwt.sign(data, process.env.SECRET_KEY);
+        res.json({ token });
       } else {
         res.json('Incorrect user or password');
       }
 
     });
 
+});
+
+router.get('/test', requireToken, (req, res) => {
+  res.json("Super secret information");
 })
+
+function requireToken(req, res, next) {
+  if (!req.headers.authorization)
+    return res.status(401).json("Unauthorized");
+
+  const token = req.headers.authorization.substr(7);
+  if (token !== '') {
+    try {
+      const content = jwt.verify(token, process.env.SECRET_KEY);
+      req.userData = content;
+      next();
+    }
+    catch {
+      return res.status(401).json("Unauthorized");
+    }
+  } else {
+    return res.status(401).json("Unauthorized");
+  }
+}
 
 module.exports = router;
