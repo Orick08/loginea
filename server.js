4f410f4da9a07001995160f6b19a56ca69d0bc2f
Erick Amezcua <erickamezcua12@gmail.com>
2021-06-05 10:17:35 -0600

    Adds init server script

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(PORT);
+console.log("Server initiated at port " + PORT);
