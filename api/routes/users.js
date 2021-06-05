6d524292e7c8d82d0c2f1aca2f30aced06e2a851
Erick Amezcua <erickamezcua12@gmail.com>
2021-06-05 11:24:14 -0600

    Adds token authorization /singup

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 
 const db = require('../model/connection');
 
@@ -14,5 +16,28 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+router.post('/singin', (req, res) => {
+  const { user, password } = req.body;
+
+  db.query('SELECT idusuario, email FROM usuarios WHERE email = ? AND password = ? LIMIT 1', [user, password],
+    (error, rows, fileds) => {
+      if (error) {
+        console.error(error);
+        res.json(error);
+        return;
+      }
+
+      if (rows.length > 0) {
+        let data = JSON.stringify(rows[0]);
+        const TOKEN = jwt.sign(data, process.env.SECRET_KEY);
+        res.json({ TOKEN });
+      } else {
+        res.json('Incorrect user or password');
+      }
+
+    });
+
+})
 
 module.exports = router;
