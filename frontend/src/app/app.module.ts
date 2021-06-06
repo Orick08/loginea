eae44c55e65b1911b9c794eb5fb8aac927bfbaf3
Erick Amezcua <erickamezcua12@gmail.com>
2021-06-05 19:00:36 -0600

    Adds initial components for angular project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PrivateComponent } from './components/private/private.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PrivateComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
